5ab8fcfcaee2d6b9963b49b556e7c42e0a0802d0
tlgms <siheun9@gmail.com>
2026-01-28 18:23:29 +0900

    feat: AuthenticationManager Bean 추가

--- src/main/java/demago/gamjappang/config/SecurityConfig.java
+++ src/main/java/demago/gamjappang/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package demago.gamjappang.config;
 import demago.gamjappang.config.oauth.PrincipalOauth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -37,5 +39,10 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
