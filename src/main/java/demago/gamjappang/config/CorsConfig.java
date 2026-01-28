6f164c69d815b6e528b277dbe49969ce5d826321
tlgms <siheun9@gmail.com>
2026-01-28 18:19:44 +0900

    feat: cors 추가 -> 추후 프론트 주소 교체 필요

---
+++ src/main/java/demago/gamjappang/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package demago.gamjappang.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.*;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        // 프론트 주소
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+
+        // 쿠키, 인증정보 포함 요청 허용
+        config.setAllowCredentials(true);
+
+//        config.setExposedHeaders(List.of("Set-Cookie"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
