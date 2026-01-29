883f6120e79f4ab30ce7bfee8263f8f62555abd7
tlgms <siheun9@gmail.com>
2026-01-29 23:27:52 +0900

    feat: 인증시 사용할 dto 추가

---
+++ src/main/java/demago/gamjappang/dto/auth/verityRequest.java
@@ -0,0 +1,12 @@
+package demago.gamjappang.dto.auth;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class verityRequest {
+    String username;
+    String AuthCode;
+}
