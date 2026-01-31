ac15965abcad9c082a4ab7c81adfe17dd6b0c8f9
tlgms <siheun9@gmail.com>
2026-01-31 22:05:42 +0900

    refactor: JwtAuthenticationFilter 추가

---
+++ src/main/java/demago/gamjappang/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,52 @@
+package demago.gamjappang.jwt;
+
+import demago.gamjappang.config.auth.PrincipalDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final PrincipalDetailsService principalDetailsService;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider,
+                                   PrincipalDetailsService principalDetailsService) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.principalDetailsService = principalDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+
+            if (jwtTokenProvider.validate(token) && SecurityContextHolder.getContext().getAuthentication() == null) {
+                String username = jwtTokenProvider.getUsername(token);
+
+                UserDetails userDetails = principalDetailsService.loadUserByUsername(username);
+
+                UsernamePasswordAuthenticationToken auth =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
