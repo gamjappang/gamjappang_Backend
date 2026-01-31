0310c0460cdd0d73ce23769fe4e0bf5d3afac851
tlgms <siheun9@gmail.com>
2026-01-31 22:08:48 +0900

    feat: UserController 추가

--- src/main/java/demago/gamjappang/controller/UserController.java
+++ src/main/java/demago/gamjappang/controller/UserController.java
@@ -14,8 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
 
 //    @GetMapping("/user/me")
 //    @PreAuthorize("isAuthenticated()")
