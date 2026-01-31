bd2355f62286f0773f95c945bcfcd5a8ed56cd2d
tlgms <siheun9@gmail.com>
2026-01-31 22:06:11 +0900

    refactor: UserController 추가

---
+++ src/main/java/demago/gamjappang/controller/UserController.java
@@ -0,0 +1,25 @@
+package demago.gamjappang.controller;
+
+import demago.gamjappang.config.auth.PrincipalDetails;
+import demago.gamjappang.model.User;
+import demago.gamjappang.rapository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+//    @GetMapping("/user/me")
+//    @PreAuthorize("isAuthenticated()")
+//    public User getUserAuthentication(){
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//    }
+}
