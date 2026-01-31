fd5bb678d406c36723c033747946a475e9130fcf
tlgms <siheun9@gmail.com>
2026-01-31 22:05:58 +0900

    refactor: AdminController 추가

---
+++ src/main/java/demago/gamjappang/controller/AdminController.java
@@ -0,0 +1,29 @@
+package demago.gamjappang.controller;
+
+import demago.gamjappang.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
+public class AdminController {
+
+    private final UserService memberService;
+
+//    // 회원 목록 조회
+//    @GetMapping
+//    public
+//    }
+//
+//    // 회원 상세 조회
+//    @GetMapping("/{id}")
+//    public String detail
+//    }
+}
